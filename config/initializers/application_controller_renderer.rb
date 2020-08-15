ab63c9518d9bb0a8f0b3befe85d6e9daa0800252
vagrant <vagrant@localhost.localdomain>
2020-08-15 13:43:42 +0000

    [Finish 5-7] meshiterro
    sns service with user function

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '9f658ff4dead312c0d293881c7523ba955ce5c2c99f67da72fba77aa36aea5a5371699553088746ed872091fd629f4af107d290fb9b0c397f4a8e216bba6cce7'
